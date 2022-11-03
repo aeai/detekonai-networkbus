61a8c678569a804725c68b3f502a4dfce68d6146
Erik.Takakura-Zajac <Erik.Takakura-Zajac@gameloft.com>
2022-11-03 17:08:51 +0100

    Fix namespace

--- Detekonai.Networking.NetworkBus/Runtime/Serializer/Experimental/INetworkSerializerFactoryProvider.cs
+++ Detekonai.Networking.NetworkBus/Runtime/Serializer/Experimental/INetworkSerializerFactoryProvider.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Detekonai.Networking.NetworkBus.Runtime.Serializer.Experimental
+namespace Detekonai.Networking.Serializer.Experimental
 {
     public interface INetworkSerializerFactoryProvider
     {
