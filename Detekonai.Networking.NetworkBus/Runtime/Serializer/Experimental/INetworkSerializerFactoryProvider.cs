f342ecf478cb254955379ddfa6be42b1c48e4af0
Erik.Takakura-Zajac <Erik.Takakura-Zajac@gameloft.com>
2022-11-03 17:05:56 +0100

    Added FactoryProvider

---
+++ Detekonai.Networking.NetworkBus/Runtime/Serializer/Experimental/INetworkSerializerFactoryProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detekonai.Networking.NetworkBus.Runtime.Serializer.Experimental
+{
+    public interface INetworkSerializerFactoryProvider
+    {
+        INetworkSerializerFactory Factory { get; }
+    }
+}
